9a54b768b13e013d2425300ccab05b6478179a1c
Raj <Rajtompe059090@gamail.com>
2026-09-12 10:18:11 +0530

    Remove leftover Google Services plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,4 @@ plugins {
   alias(libs.plugins.kotlin.compose) apply false
   alias(libs.plugins.roborazzi) apply false
   alias(libs.plugins.secrets) apply false
-  alias(libs.plugins.google.services) apply false
 }
